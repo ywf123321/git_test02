006544a1488ef27963c2072ec1df4eaf996019de
ywf <1483064314@qq.com>
2024-02-17 08:39:09 +0800

    firstcommithot-fix

--- src/main/java/com/ywf/Test.java
+++ src/main/java/com/ywf/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git11111");
         System.out.println("hello git22222");
+        System.out.println("hot-fix test1111");
     }
 }
